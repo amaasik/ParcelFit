d1d97740b128db9c3bae06d4c3246b3aa77348ee
amaasik <75305698+amaasik@users.noreply.github.com>
2022-09-08 14:17:53 +0300

    5 condition updates

--- Program.cs
+++ Program.cs
@@ -7,141 +7,151 @@ namespace toru
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
             ParcelLineFitController(BoxSizes);
         }
-        public static bool ParcelLineFitController(List<BoxSize>boxSizes)
-        {   //väikse algustähega, kuna on lokaalne muutuja
-            bool parcelFits= false;
 
-            foreach  (BoxSize box in boxSizes)
+        public static bool ParcelLineFitController(List<BoxSize> boxSizes)
+        {
+            bool parcelFits = false;
+
+            foreach (BoxSize box in boxSizes)
             {
                 Console.WriteLine("New sorting line starts");
 
                 var boxLengthInHalf = box.Length / 2;
-                var halfBoxDiagonalNotSqrt = (boxLengthInHalf * boxLengthInHalf) +
-                    (box.Width * box.Width);
+                var halfBoxDiagonalNotSqrt = (boxLengthInHalf * boxLengthInHalf) + (box.Width * box.Width);
                 var halfParcelDiagonal = Math.Sqrt(halfBoxDiagonalNotSqrt);
+                var lineWidth = 0;
 
                 foreach (SortingLineParam sortingLine in box.SortingLineParams)
                 {
-                    float lineWidth = 0;
 
                     var sortingLineNotSqrt = (sortingLine.LineWidth * sortingLine.LineWidth)
-                       + (lineWidth * lineWidth);
+                        + (lineWidth * lineWidth);
                     var cornerDiagonal = Math.Sqrt(sortingLineNotSqrt);
 
-                       if (sortingLine.LineWidth >= halfParcelDiagonal)
+                    //1
+                    if (sortingLine.LineWidth >= halfParcelDiagonal)
                     {
                         Console.WriteLine("Sorting line width is {0} and it fits", sortingLine.LineWidth);
                     }
-                    else if(sortingLine.LineWidth <= halfParcelDiagonal && lineWidth >= halfParcelDiagonal)
+                    //2
+                    else if (sortingLine.LineWidth <= halfParcelDiagonal && lineWidth >= halfParcelDiagonal)
                     {
                         Console.WriteLine("Sorting line width is {0} and it fits", sortingLine.LineWidth);
                     }
-                    else if (sortingLine.LineWidth >= box.Length || sortingLine.LineWidth >= box.Width)
+                    //3
+                    else if (box.Width <= halfParcelDiagonal && lineWidth >= halfParcelDiagonal)
                     {
                         Console.WriteLine("Sorting line width is {0} and it fits", sortingLine.LineWidth);
                     }
-                    else if(box.Width<= halfParcelDiagonal)
+                    //4
+                    else if (box.Width >= cornerDiagonal)
                     {
                         Console.WriteLine("Sorting line width is {0} and it fits", sortingLine.LineWidth);
                     }
-                    else if(sortingLine.LineWidth <= halfParcelDiagonal && sortingLine.LineWidth>= cornerDiagonal)
+                    //5
+                    else if (sortingLine.LineWidth <= halfParcelDiagonal && sortingLine.LineWidth >= cornerDiagonal)
                     {
-                        parcelFits = sortingLine.LineWidth <= halfParcelDiagonal && sortingLine.LineWidth >= cornerDiagonal;
+                        parcelFits = sortingLine.LineWidth <= halfParcelDiagonal &&
+                            sortingLine.LineWidth >= cornerDiagonal;
 
                         var result = parcelFits
-                            //? ja : lühidalt else if
-                        ? "Sorting line width is" + sortingLine.LineWidth + "and it fits" :
-                        "It dosent fit to the sorting line and needs to be wider";
+                            ? "Sorting line width is " + sortingLine.LineWidth + " and it fits" :
+                            "It dosent fit to the sorting line and needs to be wider";
+
                         Console.WriteLine(result);
                     }
+
                     else
                     {
-                        Console.WriteLine("It dosent fit to sorting line and need to be wider");
+                        Console.WriteLine("It dosent fit to the sorting line and needs to be wider");
                     }
-
-
-                
+                    lineWidth = sortingLine.LineWidth;
+                    
                 }
             }
 
             return parcelFits;
         }
+
         public static readonly List<BoxSize> BoxSizes = new List<BoxSize>
-    {
-        new BoxSize
         {
-            Length = 120,
-            Width = 60,
-            SortingLineParams = new List<SortingLineParam>
+            new BoxSize
             {
-                new SortingLineParam
+                Length = 120,
+                Width = 60,
+                SortingLineParams = new List<SortingLineParam>
                 {
-                    LineWidth = 100
-                },
-                new SortingLineParam
+                    new SortingLineParam
+                    {
+                        LineWidth = 100
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 75
+                    }
+                }
+            },
+
+            new BoxSize
+            {
+                Length = 100,
+                Width = 35,
+                SortingLineParams = new List<SortingLineParam>
                 {
-                    LineWidth= 75
+                    new SortingLineParam
+                    {
+                        LineWidth = 75
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 50
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth = 80
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 100
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 37
+                    }
+                }
+            },
+
+            new BoxSize
+            {
+                Length = 70,
+                Width = 50,
+                SortingLineParams = new List<SortingLineParam>
+                {
+                    new SortingLineParam
+                    {
+                        LineWidth = 60
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 60
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth = 55
+                    },
+                    new SortingLineParam
+                    {
+                        LineWidth= 90
+                    }
                 }
             }
-        },
-new BoxSize
-        {
-            Length = 100,
-            Width = 35,
-            SortingLineParams = new List<SortingLineParam>
-            {
-                new SortingLineParam
-                {
-                    LineWidth = 75
-                },
-                new SortingLineParam
-                {
-                    LineWidth= 50
-                },
-                new SortingLineParam
-                {
-                    LineWidth = 80
-                },
-new SortingLineParam
-                {
-                    LineWidth= 100
-                },
-                new SortingLineParam
-                {
-                    LineWidth= 37
-                }
-            }
-        },
-new BoxSize
-        {
-            Length = 70,
-            Width = 50,
-            SortingLineParams = new List<SortingLineParam>
-            {
-                new SortingLineParam
-                {
-                    LineWidth = 60
-                },
-                new SortingLineParam
-                {
-                    LineWidth= 60
-                },
-new SortingLineParam
-                {
-                    LineWidth = 55
-                },
-                new SortingLineParam
-                {
-                    LineWidth= 90
-                }
-            }
-        }
-    };
+        };
     }
 
-   
 
     public class BoxSize
     {
